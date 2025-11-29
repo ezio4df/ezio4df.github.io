f3f65b6c7a3c4a20232dcdbefbd57299b95fe532
Md. Abdul Al-Amin <ezio4df@gmail.com>
2025-11-29 11:21:26 +0600

    checkpoint

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter(),
+
+    paths: {
+      base: ''
+    }
+  }
 };
 
 export default config;
